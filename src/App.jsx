15c036973d90144630c4968a80a36b730237297f
Satellite-system <adarshurmaliya@gmail.com>
2023-11-23 02:13:27 +0530

    Update App.js for Background

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ import {
   Feedbacks,
   Hero,
   Navbar,
-  StarsCanvas,
   Tech,
   Works,
+  StarsCanvas,
 } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
