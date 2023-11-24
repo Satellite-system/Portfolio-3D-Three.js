f211134cd720f0133a1b62acc540382edd93895e
Satellite-system <adarshurmaliya@gmail.com>
2023-11-24 08:20:25 +0530

    Update About Component Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+import { Tilt } from "react-tilt";
+import { fadeIn, textVariant } from "../utils/motion";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center" >{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia itaque
+        assumenda alias nemo provident. Libero consequuntur illo fugit
+        asperiores eligendi doloremque molestiae voluptatem assumenda? Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Nulla, dolorem accusamus. Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Nihil hic facilis
+        ducimus cupiditate error! Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Lorem ipsum dolor sit amet.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
