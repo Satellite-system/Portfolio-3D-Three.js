46bf9141ba434405f30b7f17fb38d0c3eaa12bef
Satellite-system <adarshurmaliya@gmail.com>
2024-01-04 04:04:54 +0530

    Fix top-margin at 3d desktop image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,11 +24,13 @@ const Hero = () => {
         </div>
       </div>
 
+      <div className="sm:h-8"></div>
+
       {/* 3D Desktopp Image */}
       <ComputersCanvas />
 
       {/* Scrolltoggle Gif Component */}
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center ">
         <a href="#about">
           <div className="w-[35px] h-[64px] border-secondary flex justify-center items-start p-2 border-4 rounded-3xl">
             <motion.dev
